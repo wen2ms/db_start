9469e5eabd12825ee6eb72725913bc5e55c46be3
wen2ms <1819509111@qq.com>
2025-03-31 20:13:38 +0800

    Feat: aggregate functions

---
+++ aggregate_functions.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(*) FROM employee;
+
+SELECT COUNT(card_id) FROM employee;
+
+SELECT AVG(age) FROM employee;
+
+SELECT MAX(age) FROM employee;
+
+SELECT MIN(age) FROM employee;
+
+SELECT SUM(age) FROM employee WHERE work_address = '西安';
