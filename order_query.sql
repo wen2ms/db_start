edde3304b1f5c73b2ca4da0e0652cac033e00e90
wen2ms <1819509111@qq.com>
2025-03-31 20:58:58 +0800

    Feat: order query

---
+++ order_query.sql
@@ -0,0 +1,9 @@
+SELECT id,employee_id,name,gender,age,card_id,work_address,entry_date FROM employee ORDER BY age ASC;
+
+SELECT id,employee_id,name,gender,age,card_id,work_address,entry_date FROM employee ORDER BY age DESC;
+
+SELECT id,employee_id,name,gender,age,card_id,work_address,entry_date FROM employee ORDER BY age;
+
+SELECT id,employee_id,name,gender,age,card_id,work_address,entry_date FROM employee ORDER BY entry_date DESC;
+
+SELECT id,employee_id,name,gender,age,card_id,work_address,entry_date FROM employee ORDER BY age, entry_date DESC;
