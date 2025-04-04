46e57008752f1853aa55db8ff855f1cc32a3011b
wen2ms <1819509111@qq.com>
2025-04-05 00:15:55 +0800

    Feat: comlumn subquery

--- multiple_table_query.sql
+++ multiple_table_query.sql
@@ -26,7 +26,7 @@ INSERT INTO employee (id, name, age, job, salary, entry_date, manager_id, depart
 (5, '常遇春', 43, '开发', 10500, '2004-09-07', 3, 1),
 (6, '小昭', 19, '程序员鼓励师', 6600, '2004-10-12', 2, 1),
 (7, '灭绝', 60, '财务总监', 8500, '2002-09-12', 1, 3),
-(8, '周芷若', 19, '会计', 48000, '2006-06-02', 7, 3),
+(8, '周芷若', 19, '会计', 4800, '2006-06-02', 7, 3),
 (9, '丁敏君', 23, '出纳', 5250, '2009-05-13', 7, 3),
 (10, '赵敏', 20, '市场部总监', 12500, '2004-10-12', 1, 2),
 (11, '鹿杖客', 56, '职员', 3750, '2006-10-03', 10, 2),
@@ -72,3 +72,11 @@ SELECT * FROM employee WHERE age > 50;
 SELECT * FROM employee WHERE department_id = (SELECT id FROM department WHERE name = '销售部');
 
 SELECT * FROM employee WHERE entry_date > (SELECT entry_date FROM employee WHERE name = '方东白');
+
+SELECT * FROM employee WHERE department_id IN (SELECT id FROM department WHERE name = '销售部' OR name = '市场部');
+
+SELECT * FROM employee WHERE salary > 
+ALL (SELECT salary FROM employee WHERE department_id = (SELECT id FROM department WHERE name = '财务部'));
+
+SELECT * FROM employee WHERE salary >
+ANY (SELECT salary FROM employee WHERE department_id = (SELECT id FROM department WHERE name = '研发部'));
